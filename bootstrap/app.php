218dd4a1758d525b33ae1121cd3c48a0009050db
imraxy <imraxy@example.com>
2026-05-20 13:26:05 +0530

    fix: update bootstrap/app.php to use TenantServiceProvider instead of old MultitenancyServiceProvider reference

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withProviders([
         \Spatie\Multitenancy\MultitenancyServiceProvider::class,
-        \App\Providers\MultitenancyServiceProvider::class,
+        \App\Providers\TenantServiceProvider::class,
     ])
     ->create();
